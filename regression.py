d2d3ca464d4746fc6375c998723f44b69b1ef700
Csxeba <csxeba@gmail.com>
2016-04-12 16:09:08 +0200

    Fixed a bug in FFLayer @ bias updating

--- regression.py
+++ regression.py
@@ -17,13 +17,15 @@ fullpath = "fullnyers"
 
 what = "burley"
 
-crossvalrate, pca, eta, lmbd, hiddens, activationO, activationH, cost, epochs, batch_size = \
-    0.3,      10,  0.2, 0.0, (100, 30),  Sigmoid,     Sigmoid,     MSE,  20000,  20  # Burley Hypers
-#   0.3,      10,  0.3, 0.0, (100, 30),  Sigmoid,     Sigmoid,     MSE,  10000,  20  # FCV Hypers, 1st best so far
-#   0.3,      10,  0.3, 0.0, (100, 30),  Sigmoid,     Sigmoid,     MSE,  5000,   20  # FCV Hypers, 2nd best so far
-#   0.2,      10,  0.2, 0.0, (100, 30),  Sigmoid,     Sigmoid,     MSE,  20000,  20  # FCV Hypers, 3rd best so far
+crossvalrate, pca, eta, lmbd,  hiddens, activationO, activationH,   cost, epochs, batch_size = \
+    0.3,      10,  0.3, 0.0,  (100, 30),  Sigmoid,     Sigmoid,     MSE,  5000,  35  # Burley Hypers
+#   0.3,      10,  0.3, 0.0,  (100, 30),  Sigmoid,     Sigmoid,     MSE,  10000,  20  # FCV Hypers, 1st best so far
+#   0.3,      10,  0.3, 0.0,  (100, 30),  Sigmoid,     Sigmoid,     MSE,  5000,   20  # FCV Hypers, 2nd best so far
+#   0.2,      10,  0.2, 0.0,  (100, 30),  Sigmoid,     Sigmoid,     MSE,  20000,  20  # FCV Hypers, 3rd best so far
 
 runs = 10
+no_plotpoints = 200
+no_plots = 2
 
 
 def wgs_test(net: Network, on):
@@ -69,7 +71,7 @@ def build_network(data):
     return net
 
 
-def run1(queue=None, return_dynamics=False):
+def run1(queue=None, return_dynamics=False, dump=False, ID=0):
     """One run corresponds to the training of a network with randomized weights"""
     path = fcvpath if "fcv" in what.lower() else burleypath
     myData = pull_data(path)
@@ -78,13 +80,18 @@ def run1(queue=None, return_dynamics=False):
 
     for e in range(1, epochs + 1):
         network.learn(batch_size=batch_size)
-        if return_dynamics and e % (epochs // 100) == 0:
+        if return_dynamics and e % (epochs // no_plotpoints) == 0:
             terr = wgs_test(network, "testing")
             lerr = wgs_test(network, "learning")
             dynamics[0].append(terr)
             dynamics[1].append(lerr)
+            if e % ((epochs // no_plotpoints) * 10) == 0:
+                print(str(ID) + " / e: {}: T: {} L: {}".format(e, terr, lerr))
 
-    output = dynamics[0][-1], dynamics[1][-1] if not return_dynamics else dynamics
+    output = (dynamics[0][-1], dynamics[1][-1]) if not return_dynamics else dynamics
+
+    if dump:
+        dump_wgs_prediction(network, "testing", ID)
 
     if queue and return_dynamics:
         queue.put(output)
@@ -92,10 +99,11 @@ def run1(queue=None, return_dynamics=False):
         return output
 
 
-def mp_run(jobs=mp.cpu_count(), return_dynamics=False):
+def mp_run(jobs=mp.cpu_count(), return_dynamics=False, dump=False):
     """Organizes multiple runs in parallel"""
     myQueue = mp.Queue()
-    procs = [mp.Process(target=run1, args=(myQueue, return_dynamics), name="P{}".format(i)) for i in range(jobs)]
+    procs = [mp.Process(target=run1, args=(myQueue, return_dynamics, dump, i),
+                        name="P{}".format(i)) for i in range(jobs)]
     results = []
     for proc in procs:
         proc.start()
@@ -136,25 +144,23 @@ def plotted_run():
     """This experimental setup plots the run dynamics of some epochs
 
     The generated diagrams must be saved manually."""
-    X = np.arange(epochs // 50) * 50
-    f, axarr = plt.subplots(3, sharex=True)
+    X = np.arange(no_plotpoints) * (epochs // no_plotpoints)
+    f, axarr = plt.subplots(no_plots, sharex=True)
 
-    dynamics = mp_run(jobs=3, return_dynamics=True)
+    dynamics = mp_run(jobs=no_plots, return_dynamics=True, dump=True)
 
-    for i in range(3):
-        axarr[i].plot(X, dynamics[0], "r", label="T")
-        axarr[i].plot(X, dynamics[1], "b", label="L")
-        axarr[i].set_title("Run {}".format(i))
-        axarr[i].axis([0, X.max(), 0.0, 5000.0])
+    for i in range(no_plots):
+        axarr[i].plot(X, dynamics[i][0], "r", label="T")
+        axarr[i].plot(X, dynamics[i][1], "b", label="L")
+        axarr[i].axis([0, X[-1], 0.0, 5000.0])
+        axarr[i].annotate('%0.0f' % dynamics[i][0][-1], xy=(1, dynamics[i][0][-1]), xytext=(8, 0),
+                          xycoords=('axes fraction', 'data'), textcoords='offset points')
         if i == 0:
+            # axarr[i].annotate('%0.0f' % dynamics[i][0][-1], xy=(1, dynamics[i][0][-1]), xytext=(8, 0),
+            #                   xycoords=('axes fraction', 'data'), textcoords='offset points')
             axarr[i].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
                             ncol=2, mode="expand", borderaxespad=0.)
-            plt.plot(X, dynamics[1], "r", label="learning")
-        plt.plot(X, dynamics[0], "b", label="testing")
-        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-                   ncol=2, mode="expand", borderaxespad=0.)
-        plt.show()
-
+    plt.show()
 
 if __name__ == '__main__':
     plotted_run()
