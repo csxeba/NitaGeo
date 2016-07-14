6156a8a0ed4de49745a368067994935a4d6c2c50
Csxeba <csxeba@gmail.com>
2016-07-14 14:50:58 +0200

    Trying to work with NIR spectral data...

--- spectrum.py
+++ spectrum.py
@@ -1,2 +1,40 @@
-# TBD
-# regr / class based on spectral NIR data
+import sys
+
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers.core import Dense
+
+from csxnet.datamodel import CData
+from csxnet.nputils import avgpool as pool
+
+dataroot = "D:/Data/" if sys.platform == "win32" else "/data/Prog/data/"
+datapath = dataroot + "raw/nir/ntab.txt"
+
+
+def pull_data(path, avgpool=0, avg_replications=True, standardize=True):
+    myData = CData(path, cross_val=0.0)
+    if avg_replications:
+        myData.average_replications()
+    if standardize:
+        myData.standardize()
+    data = np.zeros((myData.data.shape[0], (myData.data.shape[1] // avgpool) - 2))
+    for i, line in enumerate(myData.data):
+        data[i] = pool(line, e=avgpool)
+    return data, myData.indeps
+
+
+def get_ffnn(X, Y):
+    fanin, outshape = X.shape[1], Y.shape[1]
+    model = Sequential()
+    model.add(Dense(input_dim=fanin, output_dim=120, activation="tanh"))
+    model.add(Dense(output_dim=outshape, activation="softmax"))
+    model.compile("sgd", "categorical_crossentropy")
+    return model
+
+
+if __name__ == '__main__':
+    learning, indeps = pull_data(datapath, 2, True, True)
+    y = np.eye(len(indeps))
+    network = get_ffnn(learning, y)
+    network.fit(learning, y, batch_size=10, nb_epoch=1000)
